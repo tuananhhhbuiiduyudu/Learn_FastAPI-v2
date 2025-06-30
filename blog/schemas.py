c5e6b903c1e8e612051f4b984f1fc6a3541990ff
tuananhudu <buiduytuananhudu@gmail.com>
2025-06-30 09:28:35 +0700

    hehehe

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,30 +1,45 @@
-from pydantic import BaseModel 
-from .database import Base
-from typing import List
+from typing import List, Optional
+from pydantic import BaseModel
 
-class Blog(BaseModel):
-    title : str 
-    body : str 
+
+class BlogBase(BaseModel):
+    title: str
+    body: str
+
+class Blog(BlogBase):
     class Config():
-        orm_mode = True 
-        
-        
+        orm_mode = True
+
 class User(BaseModel):
-    name : str 
-    email : str 
-    password : str
-    
+    name:str
+    email:str
+    password:str
+
 class ShowUser(BaseModel):
-    name : str 
-    email : str 
-    blogs : List[Blog] = []
+    name:str
+    email:str
+    blogs : List[Blog] =[]
     class Config():
-        orm_mode = True 
-        
-        
+        orm_mode = True
+
 class ShowBlog(BaseModel):
-    title : str
-    body : str
-    creator : ShowUser
+    title: str
+    body:str
+    creator: ShowUser
+
     class Config():
-        orm_mode = True 
+        orm_mode = True
+
+
+class Login(BaseModel):
+    username: str
+    password:str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
